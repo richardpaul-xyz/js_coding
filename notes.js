fe53ec287c89f9c8e52e12496d2ec5c46499265b
Richard Paul <richardpaulraj21@gmail.com>
2022-09-19 10:55:09 +0530

    Update notes.js

--- notes.js
+++ notes.js
@@ -1,2 +1,2 @@
-Whnever you create a variable inside a class in js you its not necessary to define var, let const 
+Whnever you create a variable or function inside a class in js you its not necessary to define var, let const ,function
 
