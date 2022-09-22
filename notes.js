b6cd7c611761bb2102fb33a70702c91e39e7ad7b
Richard Paul <richardpaulraj21@gmail.com>
2022-09-22 09:57:14 +0530

    Update notes.js

--- notes.js
+++ notes.js
@@ -1,2 +1,5 @@
 Whnever you create a variable or function inside a class in js you its not necessary to define var, let const ,function
 to make private variable inside a class use #
+
+
+1)known where to use dot notation and bracket notation
