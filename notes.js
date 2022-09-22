3cfad5963fe5f6ae055a9f7df854dadf1252a42e
Richard Paul <richardpaulraj21@gmail.com>
2022-09-22 10:18:11 +0530

    Update notes.js

--- notes.js
+++ notes.js
@@ -3,3 +3,12 @@ to make private variable inside a class use #
 
 
 1)known where to use dot notation and bracket notation
+2) function declarion dosent works in object but functio expression works
+example-
+  let richard{
+     name:'Richard',
+     age: 20,
+     roky: function(calculate){
+       return 222 - 10
+  } //this is function expression it works properly in objects
+ }
